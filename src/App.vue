160c158ae0195ee8ffa63a4436a0e2fdec5fae06
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-09-27 10:10:50 -0400

    Added Basic HTML

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,67 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="categoriesContainer">
+    <ul class="list">
+            <li>
+              <div class="text">
+                <p>Programming Languages</p>
+              </div>
+            </li>
+            <li>
+              <div class="text">
+                <p>Front-End Techs</p>
+              </div>
+            </li>
+            <li>
+              <div class="text">
+                <p>Back-End Techs</p>
+              </div>
+            </li>
+            <li>
+              <div class="text">
+                <p>Books</p>
+              </div>
+            </li>
+            <li>
+              <div class="text">
+                <p>Design</p>
+              </div>
+            </li>
+            <li>
+              <div class="text">
+                <p>Browsers</p>
+              </div>
+            </li>
+            <li>
+              <div class="text">
+                <p>Hosting</p>  
+              </div>
+            </li>
+            <li>
+              <div class="text">
+                <p>Coding Classes</p>
+              </div>
+            </li>
+            <li>
+              <div class="text">
+                <p>News</p>
+              </div>
+            </li>
+            <li>
+              <div class="text">
+                <p>Podcasts</p>
+              </div>
+            </li>
+            <li>
+              <div class="text">
+                <p>Collaboration</p>
+              </div>
+            </li>
+            <li>
+              <div class="text">
+                <p>Meetups</p>
+              </div>
+            </li>
+        </ul>
   </div>
 </template>
 
@@ -27,3 +84,6 @@
   }
 }
 </style>
+
+
+
